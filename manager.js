1ee61da9fa2c8a07b9cce7d6f49dcf8e4d612b93
flori <sf.simion.f@gmail.com>
2023-06-02 10:39:24 +0200

    solved forth query with exiting the task manager - and added error handling

--- manager.js
+++ manager.js
@@ -5,6 +5,11 @@ const fs = require("fs")
 const readline = require("readline")
 var rl = readline.createInterface(process.stdin, process.stdout)
 
+
+console.log(
+  "Welcome to your task manager, Press: \n1. to see all your tasks;\n2. to add a task;\n3. to delete a task;\n4. to Exit the task manager"
+)
+
 let tasks = ["Buy milk", "Shower", "Practice JS", "Take over the world!"];
 
 const showTasks = () => {
@@ -19,9 +24,6 @@ const deleteTask = (taskIndex) => {
   tasks.splice(taskIndex, 1)
 }
 
-console.log(
-  "Welcome to your task manager, Press: \n1. to see all your tasks;\n2. to add a task;\n3. to delete a task;\n4. to Exit the task manager"
-)
 
 
 rl.question("Pick a number", (number) => {
@@ -45,6 +47,14 @@ rl.question("Pick a number", (number) => {
         showTasks()
         rl.close()
       })
+      break
+    case "4":
+      console.log("Exiting task manager...")
+      rl.close()
+      break
+    default:
+      console.log("Invalid choice, please enter a number between 1 and 4.")
+      rl.close()
       break
   }
 })
